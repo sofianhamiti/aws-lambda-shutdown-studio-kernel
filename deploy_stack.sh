e1de0f5ba014beb5bc746eeaf7539bc8ae445527
SofianHamiti <sofian.hamiti@gmail.com>
2021-01-25 20:44:20 +0000

    fix logger typo

--- deploy_stack.sh
+++ deploy_stack.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#git clone https://github.com/SofianHamiti/aws-lambda-shutdown-studio-kernel.git
+
+#cd aws-lambda-shutdown-studio-kernel
+
 aws cloudformation create-stack \
 --stack-name studio-lambda-shutdown \
 --template-body file://template.yaml \
